c339c5e00393a93f68ae2a9e4b57e9466a89c684
Adrian Graham <adrian.a.graham@gmail.com>
2020-10-20 17:22:20 -0700

    Temp commit of in-progress bootstrap script.

--- bootstrap/orchestrate_cmd_startup.sh
+++ bootstrap/orchestrate_cmd_startup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 
 # Use this bootstrap script to install and initialize the 
 # Cloud Orchestrate API on a Debian 10 VM. It performs
@@ -8,35 +8,51 @@
 #   - Build and enter Cloud Orchestrate environment.
 #   - Deploy Cloud Orchestrate components.
 
-# TESTS
-#  - assert user is Project Owner and 'Compute Network Admin'
-#  - assert user can run sudo 
-
-export PROJECT=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+export PROJECT=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/project/project-id)
+export ZONE=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone | cut -d/ -f4)
+export BOOTSTRAPPED=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/attributes/bootstrapped)
 export ORCHESTRATE_HOME=/opt/solutions-cloud-orchestrate
-export LOCAL_REPO_LOC=/opt/orchestrate
+#export VIRTUALENV=/opt/orchestrate
 export REPO_LOC=https://github.com/GoogleCloudPlatform/solutions-cloud-orchestrate.git
 
+if [ $BOOTSTRAPPED = TRUE ]; then
+  echo "Machine already provisioned, exiting."
+  exit 0
+fi
+
 # Build .profile.
-echo 'export PROJECT=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")' >> $HOME/.profile
-echo "export ORCHESTRATE_HOME=/opt/solutions-cloud-orchestrate" >> $HOME/.profile
-echo "export LOCAL_REPO_LOC=/opt/orchestrate" >> $HOME/.profile
+echo "export PROJECT=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/project/project-id)" >> /etc/skel/.profile
+echo "export ZONE=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone | cut -d/ -f4)" >> /etc/skel/.profile
+echo "export ORCHESTRATE_HOME=/opt/solutions-cloud-orchestrate" >> /etc/skel/.profile
+#echo "export VIRTUALENV=/opt/orchestrate" >> /etc/skel/.profile
 
 # Update OS.
 sudo apt-get update
-sudo apt-get install -y git virtualenv python3-pip kubectl software-properties-common unzip
+sudo apt-get install -y git python3-pip kubectl software-properties-common unzip
 
 #
 # Build Cloud Orchestrate environment.
 #
 
-mkdir -m 0777 $LOCAL_REPO_LOC
+#mkdir -m 0777 $VIRTUALENV
 mkdir -m 0777 $ORCHESTRATE_HOME
 
-git clone $REPO_LOC $ORCHESTRATE_HOME
-virtualenv -p `which python3` $LOCAL_REPO_LOC
-source $REPO_LOC/bin/activate
+# TEMP: clone 'bootstrap' branch.
+git clone --branch bootstrap $REPO_LOC $ORCHESTRATE_HOME
+#virtualenv -p `which python3` $VIRTUALENV
+#source $REPO_LOC/bin/activate
 
-pip install google google-cloud google-cloud-pubsub google-cloud-error-reporting google-api-python-client grpcio grpcio-tools requests oauth2client setuptools_scm
+pip3 install --upgrade pip
+pip3 install google google-cloud google-cloud-pubsub google-cloud-error-reporting google-api-python-client grpcio grpcio-tools requests oauth2client setuptools_scm
 
 $ORCHESTRATE_HOME/scripts/set_project.py $PROJECT
+#$ORCHESTRATE_HOME/project/enable_apis.sh
+#$ORCHESTRATE_HOME/project/create_buckets.sh
+#$ORCHESTRATE_HOME/project/create_roles.sh
+#$ORCHESTRATE_HOME/project/create_service_accounts.sh
+#$ORCHESTRATE_HOME/project/create_topics.sh
+
+# Finally, set metadata, marking machine as complete.
+gcloud compute instances add-metadata orchestrate-cmd \
+  --metadata bootstrapped=TRUE \
+  --zone=$ZONE
