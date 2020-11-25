24053e7b7e01f15784fed3f98f0b1387b668249e
Adrian Graham <adrian.a.graham@gmail.com>
2020-11-24 16:56:06 -0800

    Reduced bootstrap to minimal lib install.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,23 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-# TODO: boilerplate
+# Copyright 2020 Google LLC
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Use this bootstrap script to install and initialize the 
-# Cloud Orchestrate API on a Debian 10 VM. It performs
-# The following tasks:
-#   - Installs core API dependencies.
+# This bootstrap installs and initializes a VM to use as a Cloud Orchestrate
+# command VM.  It performs The following tasks:
+#
+#   - Installs core Orchestrate dependencies.
 #   - Installs Cloud Orchestrate repository.
-#   - Builds and enters Cloud Orchestrate environment.
-#   - Deploy initial Cloud Orchestrate components.
 
 DATE=`date`
-SEP="***************************************************************"
+SEP="------------------------------------------------------------------"
 echo "*** START $0 $DATE ***"
 
+export CMD_NAME=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/name)
 export PROJECT=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/project/project-id)
 export ZONE=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone | cut -d/ -f4)
 export BOOTSTRAPPED=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/attributes/bootstrapped)
 export ORCHESTRATE_HOME=/opt/solutions-cloud-orchestrate
+export ORCHESTRATE_VIRTUALENV=/opt/orchestrate
 export REPO_LOC=https://github.com/GoogleCloudPlatform/solutions-cloud-orchestrate.git
 export PROFILE=/etc/profile.d/orchestrate.sh
 export BRANCH=bootstrap #main
@@ -28,19 +40,21 @@ if [ $BOOTSTRAPPED = TRUE ]; then
 fi
 
 # Build profile for all users.
-
 echo $SEP
 echo "*** Building profile.d..." 
 echo "export PROJECT=\$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/project/project-id)" >> $PROFILE
 echo "export ZONE=\$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone | cut -d/ -f4)" >> $PROFILE
 echo "export ORCHESTRATE_HOME=/opt/solutions-cloud-orchestrate" >> $PROFILE
+echo "export ORCHESTRATE_VIRTUALENV=/opt/orchestrate" >> $PROFILE
+echo "source \$ORCHESTRATE_VIRTUALENV/bin/activate" >> $PROFILE
 echo "source /usr/lib/google-cloud-sdk/completion.bash.inc" >> $PROFILE
 
 # Update OS.
 echo $SEP
 echo "*** Updating OS..."
 apt-get update
-apt-get install -y git python3-pip kubectl software-properties-common unzip acl jq
+apt-get install -y git virtualenv python3-pip kubectl \
+  software-properties-common unzip acl jq
 
 # Build Cloud Orchestrate environment.
 echo $SEP
@@ -48,97 +62,23 @@ echo "*** Provisioning $ORCHESTRATE_HOME..."
 mkdir --mode=0777 $ORCHESTRATE_HOME
 setfacl -Rdm g:adm:rw $ORCHESTRATE_HOME
 
-# Clone based on branch.
+# Build Cloud Orchestrate virtualenv.
+echo $SEP
+echo "*** Provisioning $ORCHESTRATE_VIRTUALENV..."
+mkdir --mode=0777 $ORCHESTRATE_VIRTUALENV
+setfacl -Rdm g:adm:rw $ORCHESTRATE_VIRTUALENV
+virtualenv -p `which python3` /opt/orchestrate
+chgrp -R adm $ORCHESTRATE_VIRTUALENV
+
+# Clone branch.
 echo $SEP
 echo "*** Cloning $REPO_LOC..."
 git clone --branch $BRANCH $REPO_LOC $ORCHESTRATE_HOME
 chgrp -R adm $ORCHESTRATE_HOME
 
-echo $SEP
-echo "*** Installing Python libs with pip3..."
-pip3 install --upgrade pip
-pip3 install google google-cloud google-cloud-pubsub google-cloud-error-reporting google-api-python-client grpcio grpcio-tools requests oauth2client setuptools_scm
-
-echo $SEP
-echo "$ORCHESTRATE_HOME/scripts/set_project.py $PROJECT"
-$ORCHESTRATE_HOME/scripts/set_project.py $PROJECT
-
-echo $SEP
-echo "$ORCHESTRATE_HOME/project/enable_apis.sh"
-$ORCHESTRATE_HOME/project/enable_apis.sh
-
-echo $SEP
-echo "$ORCHESTRATE_HOME/project/create_buckets.sh"
-$ORCHESTRATE_HOME/project/create_buckets.sh
-
-echo $SEP
-echo "$ORCHESTRATE_HOME/project/create_roles.sh"
-$ORCHESTRATE_HOME/project/create_roles.sh
-
-echo $SEP
-echo "$ORCHESTRATE_HOME/project/create_service_accounts.sh"
-$ORCHESTRATE_HOME/project/create_service_accounts.sh
-
-echo $SEP
-echo "$ORCHESTRATE_HOME/project/create_topics.sh"
-$ORCHESTRATE_HOME/project/create_topics.sh
-
-# echo
-# echo "$ORCHESTRATE_HOME/api/bin/create_cluster.sh"
-# $ORCHESTRATE_HOME/api/bin/create_cluster.sh
-# 
-# echo
-# echo "$ORCHESTRATE_HOME/scripts/deploy.sh"
-# $ORCHESTRATE_HOME/scripts/deploy.sh
-# 
-# echo
-# echo "$ORCHESTRATE_HOME/services/deploy.sh \
-#   $ORCHESTRATE_HOME/services/image_provisioning_start"
-# $ORCHESTRATE_HOME/services/deploy.sh \
-#   $ORCHESTRATE_HOME/services/image_provisioning_start
-# 
-# echo
-# echo "$ORCHESTRATE_HOME/services/deploy.sh \
-#   $ORCHESTRATE_HOME/services/image_provisioning_end"
-# $ORCHESTRATE_HOME/services/deploy.sh \
-#   $ORCHESTRATE_HOME/services/image_provisioning_end
-# 
-# Create API Key.
-# TODO: This command is still in alpha and is only possible on whitelisted
-# projects, according to launch/291349.
-# Therefore the script must stop here until un-blocked.
-# echo "*** Creating API key..."
-# gcloud alpha services api-keys create \
-#   --api-target=service=orchestrate.endpoints.$PROJECT.cloud.goog \
-#   --display-name="Orchestrate API key"
-# 
-# # Query values for config.
-# echo "*** Getting values for API key..."
-# KEY=$(gcloud alpha services api-keys list --format=json --filter=displayName:"Orchestrate API" | jq -r '.[0].name')
-# API_KEY=$(gcloud alpha services api-keys get-key-string --format=json $KEY | jq -r '.keyString')
-# LB_IP=$($ORCHESTRATE_HOME/api/bin/get_api_url.sh)
-# 
-# # Build .config file.
-# echo "*** Building Orchestrate .config file..."
-# mkdir -p /etc/skel/.config/orchestrate
-# cat << EOF > /etc/skel/.config/orchestrate/config_default
-# [api]
-# project = $PROJECT
-# host = $LB_IP
-# key = $API_KEY
-# EOF
-# 
-# echo "*** Compiling protos..."
-# $ORCHESTRATE_HOME/bin/compile_protos.sh
-# cd $ORCHESTRATE_HOME/cli
-# python setup.py develop
-# 
-# echo "*** Registering project..."
-# orchestrate projects register
-
 # Finally, set metadata, marking machine as complete.
 echo "*** Setting instance metadata..."
-gcloud compute instances add-metadata orchestrate-cmd \
+gcloud compute instances add-metadata $CMD_NAME \
   --metadata bootstrapped=TRUE \
   --zone=$ZONE
 
